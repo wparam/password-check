d5e4477f3cf073bc0e32964866f1b468549958c1
Jason <jason@google.com>
2020-01-19 10:03:02 -0500

    up

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const passchecker = require('./lib/index.js');
-passchecker('5dda80320799abe4606d5577653acc5faf767251fd7ad7ac4d6d896707010c32', 'test', (error, pd)=>{
+const { withcrypto,  withbcrypt  } = require('./lib/index.js');
+withcrypto('test', (error, pd)=>{
+  console.log(pd);
+  console.log('-------------------');
+});
+
+withbcrypt('test', (error, pd)=>{
   console.log(pd);
 });
