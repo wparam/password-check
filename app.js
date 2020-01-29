efb74592604213e5ef172ddfe2cff492ba9a552b
Jason <jason@google.com>
2020-01-28 19:42:45 -0500

    up

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const { withcrypto,  withbcrypt  } = require('./lib/index.js');
 withcrypto('test', (error, pd)=>{
+  console.log('-----------------------get crypt for : test----------------------------------')
   console.log(pd);
-  console.log('-------------------');
+  console.log('-----------------------------------------------------------------------------');
 });
 
-withbcrypt('test', (error, pd)=>{
+//used in node 
+withbcrypt('Electrifai@2020', (error, pd)=>{
+  console.log('-----------------------get bcrypt for : Electrifai@2020----------------------------------')
   console.log(pd);
+  console.log('-----------------------------------------------------------------------------------------')
 });
